71e77c10cbcd3c3c98d4ed0bae7ecde220ab2141
Shrut Doshi <shrutvd@gmail.com>
2023-04-11 22:20:00 +0530

    11-04-2023 22:19

--- js/homePage.js
+++ js/homePage.js
@@ -2,12 +2,18 @@ const swiper = new Swiper('.swiperMain', {
   // Optional parameters
   loop: true,
 
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 50,
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {  
+    '640': {
+      slidesPerView: 3,
+      spaceBetween: 50, },
   },
 });
